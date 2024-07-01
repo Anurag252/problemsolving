7eda151244e66ee366a5f3eae3d5cd25a0195b0b
Anurag252 <Anuragsgsits@gmail.com>
2024-07-01 12:40:52 +0200

    Time: 4 ms (56.88%) | Memory: 5.5 MB (54.13%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.c
+++ 1293-three-consecutive-odds/three-consecutive-odds.c
@@ -8,11 +8,8 @@ bool threeConsecutiveOdds(int* arr, int arrSize) {
     //qsort(arr, arrSize, sizeof(int), compare);
     int a = 0;
     int prev = 0;
-    if (arrSize <= 1) {
-        return false;
-    }
+   
     for (int i = 0 ; i < arrSize ; i ++) {
-        printf("%d\n", arr[i]);
         if (arr[i] % 2 != 0 ) {
                 a ++;
         } else {
