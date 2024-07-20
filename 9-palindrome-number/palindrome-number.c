24f8b389ffe1665478dbda149c21a732487d0fd6
Anurag252 <Anuragsgsits@gmail.com>
2024-07-20 10:27:54 +0200

    Time: 3 ms (92.99%) | Memory: 7.9 MB (9.39%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.c
@@ -0,0 +1,28 @@
+bool isPalindrome(int x) {
+    int k = 1;
+    while(x/k >= 10){
+        k = k * 10;
+    }
+    if (x < 0) {
+        return false;
+    }
+    int l = 10;
+    int y = x;
+    //printf("%d %d", x/k, y%l);
+    while(k >= 1 && y >= 1 && x/k == y%l ){
+        x = x % k;
+        k = k /10;
+        y = y / 10;
+        if (k != 0 && l != 0) {
+            //printf("%d %d\n", x/k, y%l);
+        }
+        //
+        
+    }
+
+    if (x == 0) {
+        return true;
+    }
+    return false;
+
+}
