c80da4071eacaa333a203a76b45e2c004fdd8ad4
Anurag <Anuragsgsits@gmail.com>
2024-10-01 09:12:20 +0200

    Time: 508 ms (51.15%) | Memory: 30.6 MB (27.88%) - LeetSync

--- 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.py
@@ -2,21 +2,20 @@ class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
         dic = {}
         for m in arr:
-            if k - (m%k) in dic:
-                dic[k - (m%k)] -= 1
-            else:
+                if m%k == 0:
+                    continue
                 if m%k in dic:
                     dic[m%k] += 1
                 else:
                     dic[m%k] = 1
-        print(dic)
         for m,v in dic.items():
-            if v != 0 and m != 0:
-                if k % 2 == 0 and m == k / 2 and v % 2 == 0:
-                    continue
-                else:
-                    return False
+            if m == k/2 and k % 2 == 0 and v % 2 != 0:
+                return False
+            if k-m not in dic or v != dic[k-m]:
+                return False
+
         return True
+
 
             
 
