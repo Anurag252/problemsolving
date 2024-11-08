d59c6e2c6d55675d0967fda2fa5cb5482ca6c959
Anurag <Anuragsgsits@gmail.com>
2024-11-08 19:27:38 +0530

    Time: 59 ms (53.57%) | Memory: 37.2 MB (5.52%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -2,7 +2,6 @@ class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         largestnum = 2 ** maximumBit - 1
         res = []
-        n = nums
 
 
         a =[nums[0]]
@@ -13,7 +12,7 @@ class Solution:
             a.append(a[-1] ^ k)
 
 
-        for idx, l in enumerate(a):
+        for l in a:
             res.append(largestnum - l)
         res.reverse()
         return res
