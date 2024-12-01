01f779d9f0b906de77ed6472c7b8dc3df3663d52
Anurag <Anuragsgsits@gmail.com>
2024-12-01 10:44:02 +0100

    Time: 0 ms (100.00%) | Memory: 17.4 MB (5.25%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -1,26 +1,20 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
-        arr.sort()
+        dic = set()
 
-        left = 0
-        right = len(arr) - 1
-        while(left < right):
-            if arr[left] == 0:
-                left += 1
-            s = 2 * arr[left]
-            i = bisect.bisect_left(arr[left:], s)
-            if left + i < len(arr) and arr[left+i] == s:
+        for k in arr:
+            
+            if k in dic:
                 return True
-
-            if arr[left] % 2 == 0:
-                s = arr[left] // 2
-                i = bisect.bisect_left(arr[left:], s)
-                if left + i < len(arr) and arr[left+i] == s:
-                    return True
-
-
-            left += 1
+            
+            #print(dic)
+            if k % 2 == 0: 
+                dic.add(int(k//2))
+            dic.add(k*2)
+        
         return False
+
+
 
             
 
