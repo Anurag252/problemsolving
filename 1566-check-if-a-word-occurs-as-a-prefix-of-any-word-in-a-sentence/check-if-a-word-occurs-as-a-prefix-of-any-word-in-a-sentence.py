28b5c393a50aaaab11c1099c6282cabeeb9eb451
Anurag <Anuragsgsits@gmail.com>
2024-12-02 08:52:55 +0100

    Time: 3 ms (100.00%) | Memory: 17.7 MB (5.67%) - LeetSync

---
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -0,0 +1,73 @@
+class Node:
+    def __init__(self):
+        self.end = False
+        self.mp = {}
+        self.curr = ""
+        self.count = []
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+        self.temp = self.root
+        
+
+    def insert(self, s, temp, count):
+        
+        if temp == None:
+            temp = self.root
+        print(s, temp.mp)
+        if len(s) == 0:
+            return
+        set_end = False
+        if s[0] in temp.mp:
+            temp.count.append((count, s[0]))
+            temp = temp.mp[s[0]]
+            self.insert(s[1:], temp, count)
+
+        else:
+            temp.mp[s[0]] = Node()
+            temp.curr = s[0]
+            temp.count.append((count, s[0]))
+            temp = temp.mp[s[0]]
+            print(temp)
+            self.insert(s[1:], temp, count)
+
+    def isprefix(self, s, temp):
+       
+        if temp == None:
+            temp = self.root
+            if len(s) == 1:
+                #print(temp.count)
+                for k in temp.count:
+                    if k[1] == s:
+                        return k[0]
+                return -1
+                #return min(temp.count) if s[0] in temp.mp  and len(temp.count) > 0 else -1 #and temp[s[0]].end
+
+        print(s, temp.mp, temp.count)
+        #isending = False
+        if len(s) == 1 and s[0] in temp.mp:
+                #print(temp.count)
+                for k in temp.count:
+                    if k[1] == s:
+                        return k[0]
+                return -1
+        
+        if s[0] in temp.mp:
+            return self.isprefix(s[1:], temp.mp[s[0]]) 
+        else:
+            return -1
+
+
+
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        t = Trie()
+        for idx,k in enumerate(sentence.split(' ')):
+            t.insert(k, None, idx+1)
+            #print(t)
+        return t.isprefix(searchWord, None)
+
+
+        
