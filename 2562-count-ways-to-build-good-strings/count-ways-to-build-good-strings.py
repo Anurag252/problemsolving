5c04fd6b4b26db3e178f3752df99fc1d36bf5fa0
Anurag <Anuragsgsits@gmail.com>
2024-12-30 09:24:10 +0100

    Time: 1051 ms (5.22%) | Memory: 680.1 MB (6.17%) - LeetSync

---
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.py
@@ -0,0 +1,38 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+
+
+        """
+        T[j] = 1 + T[j-zero] + T[j-one] if j >= low
+             = 0 if j > high
+             = T[j-zero] + T[j-one]
+        """
+
+        
+        T = [0] * (high+1)
+        T[zero] += 1
+        T[one] += 1
+
+        for i in range(min(zero,one),high+1):
+            if i == min(zero,one):
+                continue
+            if i > high+1:
+                T[i] = 0
+                continue
+            if i >= low+1:
+                T[i] += T[i-zero] + T[i-one]
+                continue
+            T[i] += T[i-zero] + T[i-one]
+        return sum(T[low:high+2]) % (10 ** 9 + 7)
+            
+
+        @cache
+        def recurse(count, l):
+            if l > high:
+                return 0
+            if l >= low:
+                return 1 + recurse(count , l + zero) + recurse(count , l + one) 
+            return recurse(count , l + zero) + recurse(count , l + one)
+
+        return recurse(0, 0) % (10 ** 9 + 7)
+        
