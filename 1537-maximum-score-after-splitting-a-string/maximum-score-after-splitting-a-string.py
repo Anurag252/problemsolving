5f5fa8998c0f3e8f8f56d20776e96c69d9bc0832
Anurag <Anuragsgsits@gmail.com>
2025-01-01 08:36:58 +0100

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.48%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.py
@@ -3,23 +3,21 @@ class Solution:
         res = 0
         left = []
         right = []
-        temp = 0
+        temp0 = 0
+        temp1 = 0
         for k in s:
             if k == "0":
-                temp += 1
-            left.append(temp)
-
-        temp = 0
-        for k in s[::-1]:
+                temp0 += 1
+            left.append(temp0)
             if k == "1":
-                temp += 1
-            right.append(temp)
-        right.reverse()
+                temp1 += 1
+            right.append(temp1)
+
 
         for idx, (k1, k2) in enumerate(zip(left, right)):
             if idx == len(right)-1:
                 continue
-            res = max(res, k1 + right[idx + 1])
+            res = max(res, k1 + right[-1] - right[idx])
         return res
 
 
