28cc5f3c6a1a46733d41ef58acef13c4941b7337
Anurag <Anuragsgsits@gmail.com>
2025-01-04 09:50:58 +0100

    Time: 467 ms (70.99%) | Memory: 18.4 MB (22.86%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,17 +1,6 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         arr = [(-1,-1)] * 26
-
-        st = set()
-        pref = []
-        suff = []
-        for k in s:
-            st.add(k)
-            pref.append(len(st))
-        st = set()
-        for k in s[::-1]:
-            st.add(k)
-            suff.append(len(st))
         #suff.reverse()
 
         for idx, k in enumerate(s):
@@ -19,7 +8,6 @@ class Solution:
                 arr[ord(k) - ord('a')] = (idx, -1)
             else:
                 arr[ord(k) - ord('a')] = (arr[ord(k) - ord('a')][0], idx)
-        print(arr, suff, pref)
         # there may a problem of duplicates - will not  be
         res = 0
         for (k0, k1) in arr:
