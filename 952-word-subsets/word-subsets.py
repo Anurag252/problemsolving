06055ed18167deefed6cdb391f3754bbe6f1be50
Anurag <Anuragsgsits@gmail.com>
2025-01-10 17:10:12 +0100

    Time: 487 ms (39.84%) | Memory: 21.2 MB (21.91%) - LeetSync

--- 952-word-subsets/word-subsets.py
+++ 952-word-subsets/word-subsets.py
@@ -55,7 +55,7 @@ class Solution:
             
             for i, m in enumerate(tempb):
                 bmax[i] = max(bmax[i], tempb[i])
-        print(bmax)
+
 
         res = []
         for a in A:
