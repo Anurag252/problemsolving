9d63a4e3b3879cc31ea85e24291c3f9023c2a475
Anurag <Anuragsgsits@gmail.com>
2025-01-28 21:31:30 +0100

    Time: 0 ms (100.00%) | Memory: 8.8 MB (12.50%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.go
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.go
@@ -1,13 +1,13 @@
 
-func dfs(i int, j int, grid *[][]int) int {
-    if (*grid)[i][j] == 0 {
+func dfs(i int, j int, grid [][]int) int {
+    if (grid)[i][j] == 0 {
         return 0
     }
-    res := (*grid)[i][j]
-    (*grid)[i][j] = 0
+    res := (grid)[i][j]
+    (grid)[i][j] = 0
     dir := [][]int{{1,0}, {0,1}, {-1,0}, {0,-1}}
     for _, k := range dir {
-        if k[0] + i >= 0 && k[0] + i < len(*grid) && k[1] + j >= 0 && k[1] + j < len((*grid)[0]) {
+        if k[0] + i >= 0 && k[0] + i < len(grid) && k[1] + j >= 0 && k[1] + j < len((grid)[0]) {
             res += dfs(k[0] + i, k[1] + j, grid)
         }
     }
@@ -20,7 +20,7 @@ func findMaxFish(grid [][]int) int {
         for j, k := range grid[i] {
             if k > 0 {
                // fmt.Println(grid, i, j)
-                res = max(res,dfs(i, j , &grid))
+                res = max(res,dfs(i, j , grid))
             }
         }
     }
