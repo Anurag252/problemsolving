5feb3e06c1ad05d5cfe11112561a0c9b1f80cdbc
Anurag <Anuragsgsits@gmail.com>
2025-02-03 07:24:55 +0100

    Time: 1 ms (15.56%) | Memory: 4.5 MB (64.44%) - LeetSync

---
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.go
@@ -0,0 +1,32 @@
+func longestMonotonicSubarray(nums []int) int {
+    l := 0
+    prev := -1
+    res := 0
+    for _ , k := range nums {
+        if prev < k {
+            l += 1
+            prev = k
+        } else {
+            res = max(res, l)
+            l = 1
+            prev = k
+        }
+    }
+    res = max(res, l)
+    prev = -1
+    l = 0
+    slices.Reverse(nums)
+    //fmt.Println(nums)
+    for _ , k := range nums {
+        if prev < k {
+            l += 1
+            prev = k
+        } else {
+            res = max(res, l)
+            l = 1
+            prev = k
+        }
+    }
+    res = max(res, l)
+    return res
+}
