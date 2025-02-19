c4cedefa998112bc7ec45efc7c6a2b374e2b969c
Anurag <Anuragsgsits@gmail.com>
2025-02-19 07:54:05 +0100

    Time: 18 ms (18.18%) | Memory: 9 MB (18.18%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.go
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.go
@@ -2,7 +2,7 @@
 func getHappyString(n int, k int) string {
     str := []string{"a", "b", "c"}
     // list of n is a appended to n-1 list with restrictions
-    res := calc(str, n) //T[n] = 2*T[n-1] or  4*T[n-2] or 8*T[n-3] ..., T[n-1] = 2*T[n-2] -> 2^n
+    res := calc(str, n) //T[n] = 2n*T[n-1] or  4n*T[n-2] or 8n*T[n-3] ..., T[n-1] = 2*T[n-2] -> n2^n
     // 2^10 is 1024
     //sort.Strings(res) // klogk -> saw from discussion that sorting maybe skipped as well as backtracking builds strings in correct order
     if len(res) > k - 1 {
