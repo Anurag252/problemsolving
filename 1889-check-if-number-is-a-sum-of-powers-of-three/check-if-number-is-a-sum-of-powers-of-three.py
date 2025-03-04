95e8e2206d88376dc810610ade563721fb75da97
Anurag <Anuragsgsits@gmail.com>
2025-03-04 15:05:40 +0100

    Time: 3509 ms (5.58%) | Memory: 85.6 MB (18.33%) - LeetSync

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        
+        mp = set()
+        cache = {}
+
+        def recurse(n, mp):
+            if n < 0:
+                return False
+            #print(cache)
+            if n in cache:
+                return cache[n]
+            if n == 0:
+                return True
+            
+            k = 0
+            while math.pow(3,k) <= n:
+                if k in mp:
+                    k += 1
+                    continue
+                mp.add(k)
+                if recurse(n-math.pow(3,k) , mp):
+                    cache[n] = True
+                    return True
+                mp.remove(k)
+                k += 1
+            cache[n] = False
+            return False
+        res = recurse(n, mp)
+        print(cache)
+        return res
+
+        
