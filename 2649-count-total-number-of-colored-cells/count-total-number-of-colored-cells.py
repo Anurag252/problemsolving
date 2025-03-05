2ac45167e381ce5f0010c4e093578b15bf1a9969
Anurag <Anuragsgsits@gmail.com>
2025-03-05 11:55:31 +0100

    Time: 179 ms (18.80%) | Memory: 17.5 MB (94.74%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.py
@@ -3,6 +3,13 @@ class Solution:
         """
         1, 4 +1, 8 + 4 + 1, 12 + 8 + 4 + 1
         """
+        k = 0
+        for i in range(1, n+1):
+            if i == 1:
+                k += i
+            else:
+                k += 4*(i-1)
+        return k
         if n == 1:
             return 1
 
