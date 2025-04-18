04961f71ea2bd1e8ef5a78375c3c7479d43d8842
Anurag <Anuragsgsits@gmail.com>
2025-04-18 22:53:14 +0200

    Time: 6 ms (78.80%) | Memory: 18 MB (20.58%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -1,21 +1,31 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        if n == 1:
-            return "1"
-        s = self.countAndSay(n-1)
-        k = 1
-        curr_count = 1
-        currr_elem = s[0]
-        result = ""
-        while(k < len(s)):
-            if s[k] == s[k-1]:
-                curr_count += 1
-            else:
-                result += (str(curr_count) + str(currr_elem))
-                curr_count = 1
-                currr_elem = s[k]
-            k += 1
-        result += str(curr_count) + str(currr_elem)
-        return result
+        
+        def t(n):
+            if n == 1:
+                return "1"
+            if n == 2:
+                return "11"
+            prev = ""
+            c = 0
+            res = ""
+            l = t(n-1)
+            for k in l:
+                if prev == "":
+                    prev = k
+                    c = 1
+                else:
+                    if prev == k:
+                        c += 1
+                    else:
+                        res += str(c) + prev
+                        c = 1
+                        prev = k
+            res += str(c) + prev
+            return res
+        return t(n)
+
+
+            
 
 
