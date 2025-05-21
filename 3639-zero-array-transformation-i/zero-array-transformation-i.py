55474031d6c6a350565806ea689e04b1fe0afda9
Anurag <Anuragsgsits@gmail.com>
2025-05-21 07:59:04 +0200

    Time: 174 ms (7.47%) | Memory: 55.9 MB (13.55%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.py
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.py
@@ -1,15 +1,30 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        deltaArray = [0] * (len(nums) + 1)
-        for left, right in queries:
-            deltaArray[left] += 1
-            deltaArray[right + 1] -= 1
-        operationCounts = []
-        currentOperations = 0
-        for delta in deltaArray:
-            currentOperations += delta
-            operationCounts.append(currentOperations)
-        for operations, target in zip(operationCounts, nums):
-            if operations < target:
+        """
+        every element has x numbers of reductions 
+        to find the number of reductions, process all elements and find the number of reductions
+        it will be slow if we just apply the chnages one by one
+        another way could be put -1 at the start and + 1 at the end
+
+        """
+        arr = [0] * len(nums)
+
+        for k in queries:
+            arr[k[0]] += 1
+            if k[1] + 1 < len(nums):
+                arr[k[1] + 1] -= 1
+            
+
+        subs = 0
+        for i , k in enumerate(nums):
+            subs += arr[i]
+            if nums[i] != 0:
+                nums[i] -= min(nums[i], subs)
+            
+            
+        print(nums, arr)
+        for k in nums:
+            if k != 0:
                 return False
         return True
+            
