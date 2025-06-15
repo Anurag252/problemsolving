b9d76f994cf3209d604a42839a3ca59efc5fe8c9
Anurag <Anuragsgsits@gmail.com>
2025-06-15 22:28:50 +0200

    Time: 1 ms (24.79%) | Memory: 17.9 MB (13.68%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        l = list(str(num))
+        first = 0
+        last = 0
+        for  index,k in enumerate(l):
+            if k != '9':
+                first = index
+                break
+        for  index,k in enumerate(l):
+            if k != '1' and k != '0':
+                last = index
+                break
+        high = []
+        low = []
+        for index, k in enumerate(l):
+            if k == l[first]:
+                high.append('9')
+            else:
+                high.append(k)
+
+        for index, k in enumerate(l):
+            if k == l[last]:
+                if last != 0:
+                    low.append('0')
+                else:
+                    low.append('1')
+            else:
+                low.append(k)
+        print(low, high, first, last)
+        return int("".join(high)) - int("".join(low))
+        
+        
