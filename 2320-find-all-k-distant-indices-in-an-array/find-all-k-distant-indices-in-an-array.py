9bd39a8d34951ed391cf1834e7be12e49cf83c86
Anurag <Anuragsgsits@gmail.com>
2025-06-24 13:23:19 +0200

    Time: 725 ms (5.23%) | Memory: 17.9 MB (68.60%) - LeetSync

--- 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/find-all-k-distant-indices-in-an-array.py
@@ -6,7 +6,6 @@ class Solution:
             if m == key:
                 arr.append(i)
         res = set()
-        print(arr)
         for m in arr:
             for n in range(-k, k+1):
                 if m + n >= 0 and m + n < len(nums):
