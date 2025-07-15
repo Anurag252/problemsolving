5069d6cf4d28e9adb10478e13ae3b39f01e6446b
Anurag <Anuragsgsits@gmail.com>
2025-07-15 06:00:50 +0200

    Time: 3 ms (0.00%) | Memory: 2.2 MB (42.86%) - LeetSync

---
+++ 3396-valid-word/valid-word.rs
@@ -0,0 +1,26 @@
+impl Solution {
+    pub fn is_valid(word: String) -> bool {
+        if word.len() < 3 {
+            return false;
+        }
+        let mut vowel = false;
+        let mut consonant = false;
+        for mut k in word.chars(){
+            let m = k.to_ascii_lowercase() as u8;
+            println!("{}", m);
+            if !((m >= 48 && m <= 57 ) || (m >= 97 && m <= 122) ){
+                return false;
+            } 
+            
+            if k.to_ascii_lowercase() == 'a' || k.to_ascii_lowercase() == 'e' || k.to_ascii_lowercase() == 'i' || k.to_ascii_lowercase() == 'o' || k.to_ascii_lowercase() == 'u' {
+                vowel = true;
+            }else {
+                if (m >= 97 && m <= 122) {
+                    consonant = true;
+                }
+                
+            }
+        }
+        return vowel && consonant;
+    }
+}
