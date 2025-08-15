b60e63446e996126cff27c18160ad6e312eef318
Anurag <Anuragsgsits@gmail.com>
2025-08-15 20:03:56 +0200

    Time: 0 ms (100.00%) | Memory: 2.2 MB (54.90%) - LeetSync

--- 342-power-of-four/power-of-four.rs
+++ 342-power-of-four/power-of-four.rs
@@ -1,10 +1,9 @@
 impl Solution {
     pub fn is_power_of_four(n: i32) -> bool {
        // bin of 2 is 10, 100 is 4, 1000 is 8, 10000 is 16
-       let mut k = (n as f32).log2();
-       if (k as i32) as f32 != k.ceil() {
-        return false;
-       }
-       return k as i32 % 2 == 0;
+       // so 100 ^ 011 is 0 for 2, onlt check extra if n % 3 == 0
+       return (n & (n -1)) == 0 && n % 3 == 1
+
+      
     }
 }
