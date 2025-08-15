eb5d1e81565115aa848c09eb14d64f619469c885
Anurag <Anuragsgsits@gmail.com>
2025-08-15 19:49:57 +0200

    Time: 0 ms (100.00%) | Memory: 2.9 MB (5.88%) - LeetSync

--- 342-power-of-four/power-of-four.rs
+++ 342-power-of-four/power-of-four.rs
@@ -1,16 +1,10 @@
 impl Solution {
     pub fn is_power_of_four(n: i32) -> bool {
-        if n == 1 {
-            return true;
-        }
-        if n < 1 {
-            return false;
-        }
-
-        if n % 4 == 0 {
-            return Self::is_power_of_four(n /4 as i32);
-        } else {
-            return false;
-        }
+       // bin of 2 is 10, 100 is 4, 1000 is 8, 10000 is 16
+       let mut k = (n as f32).log2();
+       if (k as i32) as f32 != k.ceil() {
+        return false;
+       }
+       return k as i32 % 2 == 0;
     }
 }
