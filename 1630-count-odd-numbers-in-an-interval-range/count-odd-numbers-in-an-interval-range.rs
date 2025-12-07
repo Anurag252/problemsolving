39798a16df36fdb73ff4a724dc1c166fdd2d8e57
Anurag <Anuragsgsits@gmail.com>
2025-12-07 23:41:19 +0100

    Time: 0 ms (100.00%) | Memory: 2.1 MB (100.00%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.rs
@@ -0,0 +1,16 @@
+impl Solution {
+    pub fn count_odds(low: i32, high: i32) -> i32 {
+        if low % 2 == 0 && high % 2 == 0 {
+            return (high - low ) / 2;
+        }
+        
+        if low % 2 == 0 || high % 2 == 0 {
+            return (high - low ) / 2 + 1;
+        }
+
+         if low % 2 != 0 && high % 2 != 0 {
+            return (high - low ) / 2 + 1;
+        }
+        0
+    }
+}
