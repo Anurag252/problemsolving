d949f780bebfdf0aad03547294400f8009a41a9b
Anurag <Anuragsgsits@gmail.com>
2025-12-23 22:29:58 +0100

    Time: 1508 ms (5.09%) | Memory: 56.5 MB (95.29%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -20,36 +20,21 @@ class Solution:
 
         ans = 0
         events.append([10**6, 10**6,0])
-        #print(events, arr)
+
 
         for idx, k in enumerate(events):
             left = idx + 1
             right = len(events) - 1
             while(left < right):
                 mid = (right + left) // 2
-                #print(mid)
                 if good(idx, mid):
                     print("yes")
                     right = mid
                 else:
                     left = mid + 1
-            #if left == len(events)-1 or left == idx:
-                #continue
             if right == len(events) - 1 :
-                ans = max(ans , k[2] )
+                ans = max(ans , k[2] ) # this is overlapping all further intervals
             else:
-                ans = max(ans , k[2] + arr[left])
+                ans = max(ans , k[2] + arr[left]) # got two maximums 
             print(idx, ans, left)
         return ans
-            
-        
-
-
-        # take a max and 2nd max
-        # or take a max only if 2nd max is
-        # start with max, if end element is less than max/2 and overlapping then we cannot find 2 elements that sum to max so ans
-        # if 2nd element is > max/2 and overlapping then count next two elements if we missed two elements
-        # that means 
-        # loop over every n and do bin search in log n inside to see if this overlaps 
-        # if it doesn't overlap move to left 
-        # if it overlaps move right
