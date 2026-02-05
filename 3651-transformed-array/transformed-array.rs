f7b61012ed8b7632a6378d83798ebdcd22336e02
Anurag <Anuragsgsits@gmail.com>
2026-02-05 22:26:25 +0100

    Time: 0 ms (100.00%) | Memory: 2 MB (100.00%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn construct_transformed_array(nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        nums.iter()
+            .enumerate()
+            .map(|(i, &num)| nums[((i as i32 + num) % n as i32 + n as i32) as usize % n])
+            .collect()
+    }
+}
