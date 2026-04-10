00df98132848c4705a3297762c26de0c76e33dd6
Anurag <Anuragsgsits@gmail.com>
2026-04-10 22:21:23 +0200

    Time: 11 ms (31.39%) | Memory: 19.2 MB (76.70%) - LeetSync

---
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        # if i sort these on nums and then on index
+        # then i slide a window of len 3
+        # and calculate the dist
+        if len(nums) < 3:
+            return -1
+        arr =  []
+        for (i, k) in enumerate(nums):
+            arr.append((k,i))
+
+        arr.sort(key = lambda x : (x[0], x[1]))
+        print(arr)
+        res = 1000000
+        left =  0
+        right = 2
+        while(right < len(arr)):
+            if arr[left][0] == arr[left + 1][0] and arr[left+1][0] == arr[right][0]:
+                res = min(res, abs(arr[left][1] - arr[left + 1][1]) + abs(arr[left + 1][1] - arr[left + 2][1]) + abs(arr[left + 2][1] - arr[left][1]))
+            right += 1
+            left += 1
+        return -1 if res == 1000000 else res
+
+
+
