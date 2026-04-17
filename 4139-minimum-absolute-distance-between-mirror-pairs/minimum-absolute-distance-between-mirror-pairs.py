ebddb257a11da723f6656db199084a07fe4e5c42
Anurag <Anuragsgsits@gmail.com>
2026-04-17 22:12:18 +0200

    Time: 351 ms (19.69%) | Memory: 41.9 MB (73.62%) - LeetSync

---
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
+        mp = {}
+        res = 1000000
+
+        for i, k in enumerate(nums):
+            t = list(str(k))
+            t.reverse()
+            t = int("".join(t))
+
+            if t % 10 == 0:
+                t = t // 10
+            
+            if k in mp:
+                res = min(res, abs(mp[k] - i))
+                mp[t] = i
+            else:
+                mp[t] = i
+            #print(t, mp, res, k)
+        return res if res < 1000000 else -1
+
+        
